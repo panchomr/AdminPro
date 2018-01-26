ab425dc97dd96a05a7cd0befb5193026e480633e
Francisco Muñoz Romero <francisco.munoz@serviex.local>
2018-01-26 16:12:13 -0300

    subiendo solucion admin pro

---
+++ src/app/shared/no-pagefound/no-pagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-pagefound',
+  templateUrl: './no-pagefound.component.html',
+  styles: []
+})
+export class NoPagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
